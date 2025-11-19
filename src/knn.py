a205224e7a31098788b31f69e9eed7f21000ec5d
Leon-Albert <leon.albert@ik.me>
2025-11-19 11:30:15 +0100

    feat: progress bar

--- src/knn.py
+++ src/knn.py
@@ -2,8 +2,9 @@ import time
 from src.ftm import rectangular_drum
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
-def find_neighbour(dataFramePath,nb_neighbour,dist,return_time=False):
+def find_neighbour(dataFramePath,nb_neighbour,dist,return_time=False,show_progress=False):
     distance_calculation = 0
     node_exploration = 0
     total_time = time.time()
@@ -27,7 +28,12 @@ def find_neighbour(dataFramePath,nb_neighbour,dist,return_time=False):
     smallest_distances = [np.inf for z in range(nb_neighbour)]
     
     #graph exploration
-    for i in range(data_size):
+    if show_progress:
+        iterator = tqdm(range(data_size))
+    else:
+        iterator = range(data_size)
+
+    for i in iterator:
         #Get phi of the neighbour
         parameterLine = data.iloc[[i]]
         theta = np.array([ parameterLine[parameters_name[k]].iloc[0] for k in range(len(parameters_name)) ])
@@ -60,21 +66,3 @@ def find_neighbour(dataFramePath,nb_neighbour,dist,return_time=False):
         return closest_neighbour,[distance_calculation,node_exploration]
     return closest_neighbour
 
-# # Example of usage
-#theta5 = [2.448304103287737,0.6724932913451673,-1.4882183960726143,-1.1237355795715704,0.9775323978804632]
-#theta6= [2.559894429686223,0.5765175855542937,-1.020964798228077,-0.1456230260198473,0.6825837860862676]
-# def createNaiveDist(theta_ref,phi):
-#     #calculation of the audio for the reference node
-#     audio_ref = rectangular_drum(theta_ref, True,**constants)
-#     phi_ref = phi(audio_ref)
-#     def naiveDistFunction(theta):
-#         audio_node = rectangular_drum(theta, True,**constants)
-#         phi_node = phi(audio_node)
-#         return torch.sqrt(torch.sum(torch.pow(torch.subtract(phi_ref, phi_node), 2), dim=0))
-#     return naiveDistFunction
-
-# def phi_test(x):
-#     return x
-    
-# dist_test = createNaiveDist(theta6,phi_test)
-# thetaList = find_neighbour('full_param_log.csv',20,dist_test,False)
