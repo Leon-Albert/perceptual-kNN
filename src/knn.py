42954c3a101ec7c3c5a4922fa3502cbfa540f175
tPerrier-collab <tomperrier59@gmail.com>
2025-11-19 08:54:05 +0100

    function find_neighbour modified and nive function

--- src/knn.py
+++ src/knn.py
@@ -1,10 +1,9 @@
 import time
 from ftm import rectangular_drum
 
-def find_neighbour(theta_ref,dataFramePath,phi,nb_neighbour,dist,return_time=False):
+def find_neighbour(dataFramePath,nb_neighbour,dist,return_time=False):
     distance_calculation = 0
     node_exploration = 0
-    phi_calculation = 0
     total_time = time.time()
     constants = {
         "x1": 0.4,
@@ -17,10 +16,9 @@ def find_neighbour(theta_ref,dataFramePath,phi,nb_neighbour,dist,return_time=Fal
         "dur":2**16
     }
     
-    data = pd.read_csv(dataFramePath)[0:1000]
-    data_size = 1000
+    data = pd.read_csv(dataFramePath)
+    data_size = data.size
     parameters_name = ["omega","tau","p","D","alpha"]
-    phi_ref = phi(theta_ref)
     
     #List initialization
     closest_neighbour =  [[0]*len(parameters_name)]*nb_neighbour
@@ -30,14 +28,11 @@ def find_neighbour(theta_ref,dataFramePath,phi,nb_neighbour,dist,return_time=Fal
     for i in range(data_size):
         #Get phi of the neighbour
         parameterLine = data.iloc[[i]]
-        theta = [ parameterLine[parameters_name[k]].iloc[0] for k in range(len(parameters_name)) ]
-        time1 = time.time()
-        phi_n = phi(theta)
-        phi_calculation += time.time() - time1
-        time1 = time.time()
-        dist_n = dist(phi_n,phi_ref)
-        distance_calculation += time.time() - time1
+        theta = np.array([ parameterLine[parameters_name[k]].iloc[0] for k in range(len(parameters_name)) ])
         
+        time1 = time.time()
+        dist_n = dist(theta)
+        distance_calculation += time.time() - time1
         time1 = time.time()
         #check if the neighbour is one of the closest
         if (dist_n < smallest_distances[-1]):
@@ -45,7 +40,6 @@ def find_neighbour(theta_ref,dataFramePath,phi,nb_neighbour,dist,return_time=Fal
             founded = False
             for k in range(nb_neighbour-2,0,-1):
                 if (dist_n > smallest_distances[k] and not(founded)):
-                    print(i)
                     smallest_distances.insert(k+1,dist_n)
                     closest_neighbour.insert(k+1,theta)
                     #Delete the furthest neighbour
@@ -61,26 +55,26 @@ def find_neighbour(theta_ref,dataFramePath,phi,nb_neighbour,dist,return_time=Fal
         node_exploration += time.time() - time1
     total_time = time.time() - total_time
     if(return_time):
-        return closest_neighbour,[distance_calculation,audio_calculation,node_exploration]
+        return closest_neighbour,[distance_calculation,node_exploration]
     return closest_neighbour
 
 theta5 = [2.448304103287737,0.6724932913451673,-1.4882183960726143,-1.1237355795715704,0.9775323978804632]
 theta6= [2.559894429686223,0.5765175855542937,-1.020964798228077,-0.1456230260198473,0.6825837860862676]
-constants = {
-    "x1": 0.4,
-    "x2": 0.4,
-    "h": 0.03,
-    "l0": np.pi,
-    "m1": 10,
-    "m2": 10,
-    "sr": 22050,
-    "dur":2**16
-}
 
-## Example of usage
+
+# # Example of usage
+# def createNaiveDist(theta_ref,phi):
+#     #calculation of the audio for the reference node
+#     audio_ref = rectangular_drum(theta_ref, True,**constants)
+#     phi_ref = phi(audio_ref)
+#     def naiveDistFunction(theta):
+#         audio_node = rectangular_drum(theta, True,**constants)
+#         phi_node = phi(audio_node)
+#         return torch.sqrt(torch.sum(torch.pow(torch.subtract(phi_ref, phi_node), 2), dim=0))
+#     return naiveDistFunction
+
 # def phi_test(x):
-#     return rectangular_drum(x, True,**constants)
-# def dist_test(x,y):
-#     return torch.sqrt(torch.sum(torch.pow(torch.subtract(x, y), 2), dim=0))
-# audio = rectangular_drum(theta3, True,**constants)
-# thetaList,calculation_time = find_neighbour(audio,'full_param_log.csv',phi_test,20,dist_test,True)
+#     return x
+    
+# dist_test = createNaiveDist(theta6,phi_test)
+# thetaList = find_neighbour('full_param_log.csv',20,dist_test,False)
